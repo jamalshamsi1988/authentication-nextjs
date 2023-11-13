145e0426181e8fc021b6e2fedd6ad42f49b419d2
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-14 00:26:05 +0330

    create sigin in page and dashbord page

---
+++ pages/dashbord.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const dashbord = () => {
+  return (
+    <div>
+      Dashbord
+    </div>
+  )
+}
+
+export default dashbord
--- pages/signin.js
+++ pages/signin.js
@@ -1,27 +1,30 @@
+import { useRouter } from "next/router";
 import { useState } from "react"
 
 
-const signup = () => {
+const signIn = () => {
     const [email , setEmail] = useState("");
-    const [password , setPassword] = useState("")
-const signUpHandler =async()=>{
-    const res=await fetch("/api/auth/signup" ,{
+    const [password , setPassword] = useState("");
+    const router=useRouter();
+
+const signInHandler =async()=>{
+    const res=await fetch("/api/auth/signin" ,{
         method : "POST",
         body : JSON.stringify({email , password}),
         headers : {"Content-Type" : "application/json"}
     })
     const data = await res.json();
-    if(data.status === "success") router.push("/signin")
+    if(data.status === "success") router.push("/dashbord")
     console.log(data)
 }
   return (
     <div>
-      <h4>Registeriation Form</h4>
+      <h4>Login Form</h4>
       <input placeholder='Email' type='text' value={email} onChange={e => setEmail(e.target.value)} />
       <input placeholder='Password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
-      <button onClick={signUpHandler}>Sing Up</button>
+      <button onClick={signInHandler}>Login</button>
     </div>
   )
 }
 
-export default signup
+export default signIn
