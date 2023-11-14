49acab2cf7a7ba8063cd7b16524e3a8dcc0b0f94
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-15 01:32:40 +0330

    dashbord is completed and connected to the api

--- pages/dashbord.js
+++ pages/dashbord.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
 import { verifyToken } from "../utils/auth";
 
-const dashbord = () => {
-  return <div>Dashbord</div>;
+const dashbord = ({ result }) => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = async () => {
+    const res = await fetch("/api/update-info", {
+      method: "POST",
+      body: JSON.stringify({ name, lastName, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+  return (
+    <div>
+      <h1>Dashbord</h1>
+      <h3>Your Email Is :{result.email}</h3>
+      <input
+        placeholder="Name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        placeholder="Last Name"
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={submitHandler}> Submit </button>
+      <h5>Your profile</h5>
+      <p>Name : {result.name}</p>
+      <p>Last Name : {result.lastName}</p>
+    </div>
+  );
 };
 
 export default dashbord;
